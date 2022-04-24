dbde795c44f1bdd760cada49b6aaace79676c509
Andrew Ezzet <andrew.ezzet@addepar.com>
2022-04-24 09:55:51 -0700

    add webpack/typescript

---
+++ webpack/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+  mode: "development",
+  devtool: 'source-map',
+  entry: {
+    content_script: path.resolve(__dirname, "..", "src", "content_script.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{from: ".", to: ".", context: "public"}]
+    }),
+  ],
+};
